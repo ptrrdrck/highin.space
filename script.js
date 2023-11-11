87a578edaccfb8039c8d20b3ccbb9b5305ac620e
trepe <56177908+ptrrdrck@users.noreply.github.com>
2023-11-10 22:39:29 -0800

    Filtering logic

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ filterOptions.forEach((option) =>
   option.addEventListener("click", handleOptionSelected)
 );
 
-document
-  .querySelector(".dropdown .title")
-  .addEventListener("change", handleTitleChange);
+//document.querySelector(".dropdown .title").addEventListener("change", handleTitleChange);
 
 function toggleClass(elem, className) {
   if (elem.className.indexOf(className) !== -1) {
@@ -40,26 +38,56 @@ function toggleDisplay(elem) {
 function toggleMenuDisplay(e) {
   const dropdown = e.currentTarget.parentNode;
   const menu = dropdown.querySelector(".menu");
-  const icon = dropdown.querySelector(".fa-angle-right");
   toggleClass(menu, "hide");
-  toggleClass(icon, "rotate-90");
 }
 
 function handleOptionSelected(e) {
   toggleClass(e.target.parentNode, "hide");
-  const id = e.target.id;
   const newValue = e.target.textContent + " ";
   const titleElem = document.querySelector(".dropdown .title");
-  const icon = document.querySelector(".dropdown .title .fa");
   titleElem.textContent = newValue;
-  titleElem.appendChild(icon);
+  const selectedOption = e.target.textContent;
   //trigger custom event
+  filterSelection(selectedOption);
   //document.querySelector(".dropdown .title").dispatchEvent(new Event("change"));
-  //setTimeout is used so transition is properly shown
-  //setTimeout(() => toggleClass(icon, "rotate-90", 0));
 }
 
-function handleTitleChange(e) {
+/*function handleTitleChange(e) {
   const result = document.getElementById("result");
   result.innerHTML = "The result is: " + e.target.textContent;
+}*/
+
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
 }
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("photo");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+filterSelection("all");
